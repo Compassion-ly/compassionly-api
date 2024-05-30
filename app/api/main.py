8e2da498a4fd0fb603ce0b795bb815e2f13c16bc
ZZIDZZ <zidanutomo@gmail.com>
2024-05-30 22:16:39 +0700

    still error, college

--- app/api/main.py
+++ app/api/main.py
@@ -1,11 +1,12 @@
 from fastapi import APIRouter
 
-from app.api.routes import auth, users, schools
+from app.api.routes import auth, users, schools, colleges
 
 api_router = APIRouter()
 api_router.include_router(auth.router, prefix="/auth", tags=["login"])
 api_router.include_router(users.router, prefix="/users", tags=["users"])
 api_router.include_router(schools.router, prefix="/schools", tags=["schools"])
+api_router.include_router(colleges.router, prefix="/colleges", tags=["colleges"])
 # api_router.include_router(utils.router, prefix="/utils", tags=["utils"])
 # api_router.include_router(items.router, prefix="/items", tags=["items"])
 
