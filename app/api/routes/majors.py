17e6129211c4a1a9d389e11bc701bfe18258dea6
Ikrar Bagaskara <ikrarb95@gmail.com>
2024-06-16 21:20:00 +0700

    FIX: predict and ADD: new logic on majors & college endpoint

--- app/api/routes/majors.py
+++ app/api/routes/majors.py
@@ -3,7 +3,8 @@ from fastapi.routing import APIRouter
 from sqlmodel import Session, select
 
 from app.core.db import get_session
-from app.models import Major,MajorListResponseModel, MajorResponseModel, MajorDetailResponseModel, Course, MajorCourse, FutureProspect, MajorDetail, MajorProspect
+from app.models import Major, MajorListResponseModel, MajorResponseModel, MajorDetailResponseModel, Course, MajorCourse, \
+    FutureProspect, MajorDetail, MajorProspect, CollegeDetail, College
 
 router = APIRouter()
 
@@ -22,12 +23,23 @@ def get_major_by_id(major_id: int, session: Session = Depends(get_session)):
     # major: Major | None = None
     # courses: List[Course] | None = None
     # prospects: List[FutureProspect] | None = None
-    major = session.get(Major, major_id)
-    # courses = session.exec(select(Course).where(Course.major_id == major_id)).all()
-    courses = session.exec(select(Course).join(MajorCourse).join(Major).where(Major.id == major_id)).all()
-    prospects = session.exec(select(FutureProspect).join(MajorProspect).where(MajorProspect.major_id == major_id)).all()
-    major_detail = MajorDetail(major=major, courses=courses, prospects=prospects)
+    # colleges: List[College] | None = None
 
+    major = session.get(Major, major_id)
     if not major:
         raise HTTPException(status_code=404, detail="Major not found")
+
+    # Retrieve courses associated with the major
+    courses = session.exec(select(Course).join(MajorCourse).join(Major).where(Major.id == major_id)).all()
+
+    # Retrieve prospects associated with the major
+    prospects = session.exec(select(FutureProspect).join(MajorProspect).where(MajorProspect.major_id == major_id)).all()
+
+    # Retrieve colleges that offer the major
+    college_ids = session.exec(select(CollegeDetail.college_id).where(CollegeDetail.major_id == major_id)).all()
+    colleges = session.exec(select(College).where(College.id.in_(college_ids))).all()
+
+    # Create the major detail object including the colleges
+    major_detail = MajorDetail(major=major, courses=courses, prospects=prospects, colleges=colleges)
+
     return MajorDetailResponseModel(message="success", data=major_detail)
